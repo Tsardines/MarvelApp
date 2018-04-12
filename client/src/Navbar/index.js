90f0d768b88a85c6a70530c76a805395afc6d151
Haley Sulcer <hjsulcer@gmail.com>
2018-04-11 20:58:40 -0400

    created navbar folder, index.js, and style.css. Created index.js content

---
+++ client/src/Navbar/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+// home favorites login signup
+
+class SiteNav extends Component {
+  render() {
+    return (
+      <ul className="nav">
+
+        <li className="nav-item">
+          <h1 className="site-logo">Marvel App</h1>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/favorites/" className="nav-link">
+            Favorites
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link to="/signup" className="nav-link">
+            Sign Up
+          </Link>
+        </li>
+
+      </ul>
+    );
+  }
+}
+
+export default SiteNav;
