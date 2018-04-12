863cf8ac98b306acb0eafb62b1ce625fd80a642b
Haley Sulcer <hjsulcer@gmail.com>
2018-04-11 23:17:46 -0400

    Register index.js  'works' in browser, added <Register to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Register from "./Register";
 import './App.css';
 
 class App extends Component {
+  state = {
+    fields: {}
+  };
+
+  onChange = updatedValue => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        ...updatedValue
+      }
+    });
+  };
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <Register onChange={fields => this.onChange(fields)} />
+        <p>
+          {JSON.stringify(this.state.fields, null, 2)}
         </p>
       </div>
     );
