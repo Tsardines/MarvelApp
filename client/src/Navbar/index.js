e51de5d470211b31f0982d72bce0cfad62fa3665
Haley Sulcer <hjsulcer@gmail.com>
2018-04-13 10:47:08 -0400

    navbar (mostly) bootstrapped

--- client/src/Navbar/index.js
+++ client/src/Navbar/index.js
@@ -1,39 +1,96 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./style.css";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-class Navbar extends Component {
-  render() {
-    return (
-      <ul className="nav">
 
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            HOME
-          </Link>
-        </li>
+  class Bar extends React.Component {
+    constructor(props) {
+      super(props);
 
-        <li className="nav-item">
-          <Link to="/favorites/" className="nav-link">
-            FAVORITES
-          </Link>
-        </li>
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        isOpen: false
+      };
+    }
+    toggle() {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
+    render() {
+      return (
+        <div>
+          <Navbar color="dark" expand="md">
+            <NavbarBrand href="/">Marvel Character Finder</NavbarBrand>
 
-        <li className="nav-item">
-          <Link to="/login" className="nav-link">
-            LOGIN
-          </Link>
-        </li>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/">Home</NavLink>
+                </NavItem>
 
-        <li className="nav-item">
-          <Link to="/register" className="nav-link">
-            REGISTER
-          </Link>
-        </li>
+                <NavItem>
+                  <NavLink href="/favorites">Favorites</NavLink>
+                </NavItem>
 
-      </ul>
-    );
+                <NavItem>
+                  <NavLink href="/login">Login</NavLink>
+                </NavItem>
+
+                <NavItem>
+                  <NavLink href="/register">Register</NavLink>
+                </NavItem>
+              </Nav>
+            </Navbar>
+        </div>
+      );
+    }
   }
-}
 
-export default Navbar;
+
+// class Navbar extends Component {
+//   render() {
+//     return (
+//       <ul className="nav">
+//
+//         <li className="nav-item">
+//           <Link to="/" className="nav-link">
+//             HOME
+//           </Link>
+//         </li>
+//
+//         <li className="nav-item">
+//           <Link to="/favorites/" className="nav-link">
+//             FAVORITES
+//           </Link>
+//         </li>
+//
+//         <li className="nav-item">
+//           <Link to="/login" className="nav-link">
+//             LOGIN
+//           </Link>
+//         </li>
+//
+//         <li className="nav-item">
+//           <Link to="/register" className="nav-link">
+//             REGISTER
+//           </Link>
+//         </li>
+//
+//       </ul>
+//     );
+//   }
+// }
+
+export default Bar;
