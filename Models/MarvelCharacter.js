cb206496bf5c3fb3cc88f8b4f63edfa537ede07b
Pete LaMalfa <lamalfa.peter@gmail.com>
2018-04-13 09:28:54 -0400

    added to character model

--- Models/MarvelCharacter.js
+++ Models/MarvelCharacter.js
@@ -1,13 +1,13 @@
-// const db = require("../database/connection");
-//
-// const MarvelCharacter = {};
-// MarvelCharacter.addCharacterToDatabase = character_id => {
-//   return db.one(
-//     "INSERT INTO marvel_character (marvel_id, name, description, image_url, series, wiki_url)
-//       VALUES ($1, $2, $3, $4, $5, $6)",
-//
-//     )
-//   )
-// }
-//
-// module.exports = MarvelCharacter;
+const db = require("../database/connection");
+
+const MarvelCharacter = {};
+MarvelCharacter.addCharacterToDatabase = character_id => {
+  return db.one(
+    "INSERT INTO marvel_character (marvel_id, name, description, image_url, series, wiki_url)
+      VALUES ($1, $2, $3, $4, $5, $6)",
+      [marvel_id, name, description, image_url, series, wiki_url]
+    )
+  )
+}
+
+module.exports = MarvelCharacter;
